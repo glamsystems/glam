ea706b763149e1b3b3473ea84cba83a67dc637cf
Youssef Srondy <srondyyoussef@gmail.com>
2024-03-19 09:09:30 +0100

    initial progress, products page with dummy data + header + connect wallet button.

--- web/src/app/account/account-list-feature.tsx
+++ web/src/app/account/account-list-feature.tsx
@@ -1,6 +1,6 @@
-import { useWallet } from '@solana/wallet-adapter-react';
-import { WalletButton } from '../solana/solana-provider';
 import { Navigate } from 'react-router-dom';
+import { WalletButton } from '../solana/solana-provider';
+import { useWallet } from '@solana/wallet-adapter-react';
 
 export default function AccountListFeature() {
   const { publicKey } = useWallet();
@@ -12,7 +12,7 @@ export default function AccountListFeature() {
   return (
     <div className="hero py-[64px]">
       <div className="hero-content text-center">
-        <WalletButton />
+        <WalletButton>x</WalletButton>
       </div>
     </div>
   );
