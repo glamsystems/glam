a00310b882441fb169f721ea752f4308305f6158
Emanuele Cesena <emanuele.cesena@gmail.com>
2024-03-31 12:44:44 -0700

    share: metadata draft

--- anchor/programs/glam/src/instructions/manager.rs
+++ anchor/programs/glam/src/instructions/manager.rs
@@ -1,5 +1,7 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, system_program};
+use anchor_spl::token_2022;
 use anchor_spl::token_interface::{Mint, Token2022};
+use spl_token_2022::{extension::ExtensionType, state::Mint as StateMint};
 
 use crate::error::ManagerError;
 use crate::state::fund::*;
@@ -15,14 +17,18 @@ pub struct InitializeFund<'info> {
 
     // this can be either token or token2022
     // pub asset_base: InterfaceAccount<'info, Mint>,
-    #[account(init, seeds = [b"share-0".as_ref(), fund.key().as_ref()], bump, payer = manager, mint::decimals = 9, mint::authority = share)]
-    pub share: Box<InterfaceAccount<'info, Mint>>,
+    // #[account(init, seeds = [b"share-0".as_ref(), fund.key().as_ref()], bump, payer = manager, mint::decimals = 9, mint::authority = share)]
+    // pub share: Box<InterfaceAccount<'info, Mint>>,
+    /// CHECK: test
+    #[account(mut, seeds = [b"share-0".as_ref(), fund.key().as_ref()], bump)]
+    pub share: AccountInfo<'info>,
 
     #[account(mut)]
     pub manager: Signer<'info>,
 
     pub system_program: Program<'info, System>,
     pub token_program: Program<'info, Token2022>,
+    pub rent: Sysvar<'info, Rent>,
 }
 
 pub fn initialize_fund_handler<'c: 'info, 'info>(
@@ -69,6 +75,91 @@ pub fn initialize_fund_handler<'c: 'info, 'info>(
     treasury.manager = ctx.accounts.manager.key();
     treasury.fund = fund.key();
     treasury.bump = ctx.bumps.treasury;
+
+    // share class
+    let share_mint = ctx.accounts.share.to_account_info();
+    let share_metadata = ctx.accounts.share.to_account_info();
+    let share_mint_authority = ctx.accounts.share.to_account_info();
+    let share_metadata_authority = ctx.accounts.share.to_account_info();
+
+    let fund_key = ctx.accounts.fund.key();
+    let seeds = &[
+        "share-0".as_bytes(),
+        fund_key.as_ref(),
+        &[ctx.accounts.fund.share_classes_bumps[0]],
+    ];
+    let signer_seeds = &[&seeds[..]];
+
+    let space =
+        ExtensionType::try_calculate_account_len::<StateMint>(&[ExtensionType::MetadataPointer])
+            .unwrap();
+    let meta_data_space = 250;
+
+    let lamports_required = (Rent::get()?).minimum_balance(space + meta_data_space);
+
+    msg!(
+        "Create Mint and metadata account size and cost: {} lamports: {}",
+        space as u64,
+        lamports_required
+    );
+
+    system_program::create_account(
+        CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            system_program::CreateAccount {
+                from: ctx.accounts.manager.to_account_info(),
+                to: share_mint.clone(),
+            },
+            signer_seeds,
+        ),
+        lamports_required,
+        space as u64,
+        &ctx.accounts.token_program.key(),
+    )?;
+
+    // Initialize the metadata pointer (Need to do this before initializing the mint)
+    let init_meta_data_pointer_ix =
+        spl_token_2022::extension::metadata_pointer::instruction::initialize(
+            &Token2022::id(),
+            &share_mint.key(),
+            Some(share_metadata_authority.key()),
+            Some(share_metadata.key()),
+        )?;
+    solana_program::program::invoke(
+        &init_meta_data_pointer_ix,
+        &[share_mint.clone(), share_metadata_authority.clone()],
+    )?;
+
+    // mint2
+    let mint_cpi_ix = CpiContext::new(
+        ctx.accounts.token_program.to_account_info(),
+        token_2022::InitializeMint2 {
+            mint: share_mint.clone(),
+        },
+    );
+    token_2022::initialize_mint2(mint_cpi_ix, 0, &share_mint_authority.key(), None).unwrap();
+
+    // Init the metadata account
+    let init_token_meta_data_ix = spl_token_metadata_interface::instruction::initialize(
+        &spl_token_2022::id(),
+        &share_metadata.key(),
+        &share_metadata_authority.key(),
+        &share_mint.key(),
+        &share_mint_authority.key(),
+        "Beaver".to_string(),
+        "BVA".to_string(),
+        "https://arweave.net/MHK3Iopy0GgvDoM7LkkiAdg7pQqExuuWvedApCnzfj0".to_string(),
+    );
+    solana_program::program::invoke_signed(
+        &init_token_meta_data_ix,
+        &[
+            share_metadata,
+            share_metadata_authority,
+            share_mint,
+            share_mint_authority,
+        ],
+        signer_seeds,
+    )?;
 
     msg!("Fund created: {}", ctx.accounts.fund.key());
     Ok(())
