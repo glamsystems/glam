b897029be04f98e381206e057af8ec8ddd4cb56f
Yuru Shao <shaoyuru@gmail.com>
2024-04-03 00:42:50 -0700

    api: set up tests with jest and supertest (#39)

---
+++ api/tests/server.test.js
@@ -0,0 +1,25 @@
+const server = require("../server.js");
+const supertest = require("supertest");
+const requestWithSupertest = supertest(server);
+
+describe("Test /metadata/:pubkey", () => {
+  afterAll(() => {
+    server.close();
+  });
+
+  it("Invalid pubkey", async () => {
+    const res = await requestWithSupertest.get("/metadata/xyz");
+    expect(res.status).toEqual(404);
+  });
+});
+
+describe("Test /fund/:pubkey/perf", () => {
+  afterAll(() => {
+    server.close();
+  });
+
+  it("", async () => {
+    const res = await requestWithSupertest.get("/fund/xyz/perf");
+    expect(res.status).toEqual(200);
+  });
+});
