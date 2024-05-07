16e97bb0082bf5b968fea39ed4a1d7181e7a0a9b
yurushao <shaoyuru@gmail.com>
2024-05-07 00:42:12 -0700

    Add tests to api/

---
+++ api/tests/server.test.ts
@@ -0,0 +1,16 @@
+import exp from "constants";
+import server from "../src/main.js";
+import supertest from "supertest";
+const requestWithSupertest = supertest(server);
+
+describe("Test /api", () => {
+  afterAll(() => {
+    server.close();
+  });
+
+  it("test", async () => {
+    const res = await requestWithSupertest.get("/api");
+    expect(res.status).toEqual(200);
+    expect(res.body).toEqual({ message: "Welcome to api!" });
+  });
+});
