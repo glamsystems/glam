9f6254f0f8a3a75f91f0f9b9a0467a0716d256df
yurushao <shaoyuru@gmail.com>
2024-05-08 23:56:53 -0700

    remove webapp dependencies

--- deploy_api.sh
+++ deploy_api.sh
@@ -15,9 +15,46 @@ pnpm install
 pnpm run api-build
 
 cp api/app.yaml $DIST/app.yaml
-cp pnpm-lock.yaml $DIST/pnpm-lock.yaml
-jq '.scripts +{"gcp-build": ""} +{"start": "node main.js"}' package.json  > $DIST/package.json
+# cp pnpm-lock.yaml $DIST/pnpm-lock.yaml
 
+jq '.scripts +={"gcp-build": ""} +{"start": "node main.js"}' package.json  > $DIST/tmp_package.json
+
+# Delete unnecessary dependencies for the api to speed up the deploy and reduce the size of build artifact
+deps_to_remove=(
+  "@carbon/charts-react"
+  "@carbon/colors"
+  "@carbon/icons-react"
+  "@carbon/react"
+  "@carbon/styles"
+  "@carbon/type"
+  "@ibm/plex"
+  "@hookform/resolvers"
+  "@solana-developers/helpers"
+  "@solana-developers/preset-react"
+  "@swc/helpers"
+  "@tabler/icons-react"
+  "@tailwindcss/typography"
+  "@tanstack/react-query"
+  "carbon-components"
+  "carbon-components-react"
+  "carbon-icons"
+  "daisyui"
+  "react"
+  "react-dom"
+  "react-hook-form"
+  "react-hot-toast"
+  "react-router-dom"
+  "use-dark-mode"
+  "zod"
+)
+jq_command=". | del(.devDependencies)"
+# Build the jq command to delete each key in the list
+for key in "${deps_to_remove[@]}"; do
+  jq_command+=" | del(.dependencies[\"$key\"])"
+done
+jq "$jq_command" $DIST/tmp_package.json > $DIST/package.json
+
+rm $DIST/tmp_package.json
 cd $DIST
 
 if [ $TARGET == "dev" ]; then
