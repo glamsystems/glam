a0f41f9ed11925d147d877fbf9c4b32819ce5086
Emanuele Cesena <emanuele.cesena@gmail.com>
2024-06-01 14:40:32 -0700

    swap: local test

--- anchor/src/client/jupiter.ts
+++ anchor/src/client/jupiter.ts
@@ -1,9 +1,11 @@
+const util = require("util");
 import * as anchor from "@coral-xyz/anchor";
 import {
   AddressLookupTableAccount,
   PublicKey,
   TransactionInstruction,
   TransactionSignature,
+  Transaction,
   TransactionMessage,
   VersionedTransaction
 } from "@solana/web3.js";
@@ -49,6 +51,7 @@ export class JupiterClient {
     manager: PublicKey,
     quote?: any,
     quoteResponse?: any,
+    swapTransaction?: any,
     swapInstruction?: any,
     addressLookupTableAddresses?: string[]
   ): Promise<VersionedTransaction> /* MethodsBuilder<Glam, ?> */ {
@@ -57,47 +60,68 @@ export class JupiterClient {
         /* Fetch quoteResponse is not specified */
         console.log("Fetching quoteResponse...");
         quoteResponse = await this.getQuoteResponse(quote);
+        console.log("quoteResponse", util.inspect(quoteResponse, false, null));
       }
       /* Fetch swapInstruction is not specified */
-      console.log("Fetching swapInstruction...");
-      [swapInstruction, addressLookupTableAddresses] =
-        await this.getSwapInstruction({
-          userPublicKey: manager,
-          quoteResponse
-        });
+      // console.log("Fetching swapInstruction...");
+      // const treasury = this.base.getTreasuryPDA(fund);
+      // [swapInstruction, addressLookupTableAddresses] =
+      //   await this.getSwapInstruction({
+      //     // XXX
+      //     userPublicKey: manager,
+      //     wrapAndUnwrapSol: false,
+      //     quoteResponse
+      //   });
+      swapTransaction = await this.getSwapTransaction({
+        userPublicKey: manager,
+        // wrapAndUnwrapSol: false,
+        // asLegacyTransaction: true,
+        quoteResponse
+      });
     }
+    // XXX
+    // swapInstruction.keys[1].isSigner = false;
     // console.log("swapInstruction", swapInstruction);
     /* Create the tx for jupiterSwap using the swapInstruction */
-    const tx = await this.base.program.methods
-      .jupiterSwap(swapInstruction.data)
-      .accounts({
-        fund,
-        manager,
-        treasury: this.base.getTreasuryPDA(fund),
-        jupiterProgram
-      })
-      .remainingAccounts(swapInstruction.keys)
-      .transaction();
+    // const tx = await this.base.program.methods
+    //   .jupiterSwap(swapInstruction.data)
+    //   .accounts({
+    //     fund,
+    //     manager,
+    //     treasury: this.base.getTreasuryPDA(fund),
+    //     jupiterProgram
+    //   })
+    //   .remainingAccounts(swapInstruction.keys)
+    //   .transaction();
 
-    const connection = this.base.provider.connection;
-    const lookupTableAccounts = (
-      await Promise.all(
-        (addressLookupTableAddresses || []).map(
-          async (address) =>
-            (
-              await connection.getAddressLookupTable(new PublicKey(address))
-            ).value
-        )
-      )
-    ).filter((x) => !!x) as AddressLookupTableAccount[];
+    // const connection = this.base.provider.connection;
+    // const lookupTableAccounts = (
+    //   await Promise.all(
+    //     (addressLookupTableAddresses || []).map(
+    //       async (address) =>
+    //         (
+    //           await connection.getAddressLookupTable(new PublicKey(address))
+    //         ).value
+    //     )
+    //   )
+    // ).filter((x) => !!x) as AddressLookupTableAccount[];
 
-    const messageV0 = new TransactionMessage({
-      payerKey: manager,
-      recentBlockhash: (await connection.getLatestBlockhash()).blockhash,
-      instructions: tx.instructions
-    }).compileToV0Message(lookupTableAccounts);
+    // const tx = Transaction.from(swapTransaction);
+    // const messageV0 = new TransactionMessage({
+    //   payerKey: manager,
+    //   recentBlockhash: (
+    //     await this.base.provider.connection.getLatestBlockhash()
+    //   ).blockhash,
+    //   instructions: tx.instructions
+    // }).compileToV0Message();
+    // const vTx = new VersionedTransaction(messageV0);
 
-    return new VersionedTransaction(messageV0);
+    const vTx = VersionedTransaction.deserialize(swapTransaction);
+    vTx.message.recentBlockhash = (
+      await this.base.provider.connection.getLatestBlockhash()
+    ).blockhash;
+    console.log(util.inspect(vTx, false, null));
+    return vTx;
   }
 
   public async getQuoteResponse(quote: any): Promise<any> {
@@ -108,6 +132,20 @@ export class JupiterClient {
     const quoteResponse = await res.json();
     // console.log("quoteResponse", quoteResponse);
     return quoteResponse;
+  }
+
+  public async getSwapTransaction(quoteResponse: any): Promise<any> {
+    const res = await fetch(`${this.base.jupiterApi}/swap`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(quoteResponse)
+    });
+
+    const data = await res.json();
+    return Buffer.from(data.swapTransaction, "base64");
   }
 
   public async getSwapInstructions(quoteResponse: any): Promise<any> {
@@ -124,14 +162,16 @@ export class JupiterClient {
     return instructions;
   }
 
-  async getSwapInstruction(
-    quoteResponse: any
-  ): Promise<[TransactionInstruction, string[]]> {
-    const { swapInstruction, addressLookupTableAddresses } =
-      await this.getSwapInstructions(quoteResponse);
+  async getSwapInstruction(quoteResponse: any): Promise<{
+    swapInstruction: TransactionInstruction;
+    addressLookupTableAddresses: string[];
+  }> {
+    const res = await this.getSwapInstructions(quoteResponse);
+    console.log("res", res);
 
-    return [
-      new TransactionInstruction({
+    const { swapInstruction, addressLookupTableAddresses } = res;
+    return {
+      swapInstruction: new TransactionInstruction({
         programId: new PublicKey(swapInstruction.programId),
         keys: swapInstruction.accounts.map((key: any) => ({
           pubkey: new PublicKey(key.pubkey),
@@ -141,7 +181,7 @@ export class JupiterClient {
         data: Buffer.from(swapInstruction.data, "base64")
       }),
       addressLookupTableAddresses
-    ];
+    };
   }
 
   /*
--- anchor/tests/glam_jupiter.spec.ts
+++ anchor/tests/glam_jupiter.spec.ts
@@ -18,13 +18,107 @@ describe("glam_jupiter", () => {
   });
 
   it("Swap", async () => {
+    // TODO: download accounts so this also works
+    const quoteResponseSaber = {
+      inputMint: "So11111111111111111111111111111111111111112",
+      inAmount: "100000000",
+      outputMint: "mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So",
+      outAmount: "83883238",
+      otherAmountThreshold: "83463822",
+      swapMode: "ExactIn",
+      slippageBps: 50,
+      platformFee: null,
+      priceImpactPct: "0.0000048124147404582633488403",
+      routePlan: [
+        {
+          swapInfo: {
+            ammKey: "Lee1XZJfJ9Hm2K1qTyeCz1LXNc1YBZaKZszvNY4KCDw",
+            label: "Saber",
+            inputMint: "So11111111111111111111111111111111111111112",
+            outputMint: "mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So",
+            inAmount: "100000000",
+            outAmount: "83883238",
+            feeAmount: "8389",
+            feeMint: "mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So"
+          },
+          percent: 100
+        }
+      ],
+      contextSlot: 269276632,
+      timeTaken: 0.005332463
+    };
+
+    // TODO: download accounts so this also works
+    const quoteResponseMeteora = {
+      inputMint: "So11111111111111111111111111111111111111112",
+      inAmount: "100000000",
+      outputMint: "mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So",
+      outAmount: "83884254",
+      otherAmountThreshold: "83464833",
+      swapMode: "ExactIn",
+      slippageBps: 50,
+      platformFee: null,
+      priceImpactPct: "0.0001301870270953763480211372",
+      routePlan: [
+        {
+          swapInfo: {
+            ammKey: "HcjZvfeSNJbNkfLD4eEcRBr96AD3w1GpmMppaeRZf7ur",
+            label: "Meteora",
+            inputMint: "So11111111111111111111111111111111111111112",
+            outputMint: "mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So",
+            inAmount: "100000000",
+            outAmount: "83884254",
+            feeAmount: "10000",
+            feeMint: "So11111111111111111111111111111111111111112"
+          },
+          percent: 100
+        }
+      ],
+      contextSlot: 269284604,
+      timeTaken: 0.002404066
+    };
+
+    const quoteResponseMercurial = {
+      inputMint: "So11111111111111111111111111111111111111112",
+      inAmount: "100000000",
+      outputMint: "mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So",
+      outAmount: "83882593",
+      otherAmountThreshold: "83463181",
+      swapMode: "ExactIn",
+      slippageBps: 50,
+      platformFee: null,
+      priceImpactPct: "0.0000227964401766161245124881",
+      routePlan: [
+        {
+          swapInfo: {
+            ammKey: "MAR1zHjHaQcniE2gXsDptkyKUnNfMEsLBVcfP7vLyv7",
+            label: "Mercurial",
+            inputMint: "So11111111111111111111111111111111111111112",
+            outputMint: "mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So",
+            inAmount: "100000000",
+            outAmount: "83882593",
+            feeAmount: "8389",
+            feeMint: "mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So"
+          },
+          percent: 100
+        }
+      ],
+      contextSlot: 269285001,
+      timeTaken: 0.002353586
+    };
+
     try {
-      const txId = await glamClient.jupiter.swap(fundPDA, {
-        inputMint: wSol,
-        outputMint: mSol,
-        amount: 500_000_000,
-        maxAccounts: 10
-      });
+      const txId = await glamClient.jupiter.swap(
+        fundPDA,
+        {
+          inputMint: wSol,
+          outputMint: mSol,
+          amount: 100_000_000,
+          onlyDirectRoutes: true,
+          maxAccounts: 8 // this should get Mercurial
+        },
+        quoteResponseMercurial // this forces Mercurial
+      );
       console.log("swap txId", txId);
     } catch (e) {
       console.error(e);
