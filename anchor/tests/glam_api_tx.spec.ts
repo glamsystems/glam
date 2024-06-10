1938ef207a0d310399ff226079417c53badf6c81
Yuru Shao <shaoyuru@gmail.com>
2024-06-09 22:36:59 -0700

    Send tx with sendAndConfirmTransaction

--- anchor/tests/glam_api_tx.spec.ts
+++ anchor/tests/glam_api_tx.spec.ts
@@ -1,7 +1,6 @@
 import * as anchor from "@coral-xyz/anchor";
 import {
   Transaction,
-  SystemProgram,
   sendAndConfirmTransaction,
   PublicKey,
   VersionedTransaction
@@ -12,78 +11,63 @@ import { GlamClient } from "../src/client";
 const API = "http://localhost:8080";
 const wsol = new PublicKey("So11111111111111111111111111111111111111112");
 const msol = new PublicKey("mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So");
-const usdc = new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
+const manager = "gLJHKPrZLGBiBZ33hFgZh6YnsEhTVxuRT17UCqNp6ff";
+const fund = "4gAcSdfSAxVPcxj2Hi3AvKKViGat3iUysDD5ZzbqhDTk";
 
 describe("glam_api_tx", () => {
   const glamClient = new GlamClient();
 
-  /*
-  it("Wrap sol", async () => {
+  it("Wrap 0.001 sol", async () => {
     const response = await fetch(`${API}/tx/wsol/wrap`, {
       method: "POST",
       headers: { "content-type": "application/json" },
-      body: JSON.stringify({
-        manager: "gLJHKPrZLGBiBZ33hFgZh6YnsEhTVxuRT17UCqNp6ff",
-        fund: "4gAcSdfSAxVPcxj2Hi3AvKKViGat3iUysDD5ZzbqhDTk",
-        amount: 1000000
-      })
+      body: JSON.stringify({ manager, fund, amount: 1000000 })
     });
     const { tx } = await response.json();
     console.log("Wrap tx:", tx);
-    const t = Transaction.from(Buffer.from(tx, "hex"));
-    t.sign(glamClient.getWalletSigner());
+
     try {
-      const txId = await glamClient.provider.connection.sendRawTransaction(
-        t.serialize()
+      const txId = await sendAndConfirmTransaction(
+        glamClient.provider.connection,
+        Transaction.from(Buffer.from(tx, "hex")),
+        [glamClient.getWalletSigner()]
       );
       console.log("Wrap txId:", txId);
     } catch (error) {
       console.log("Error", error);
       throw error;
     }
-  });
+  }, 30_000);
 
-  it("unwrap", async () => {
+  it("Unwrap wsol", async () => {
     const response = await fetch(`${API}/tx/wsol/unwrap`, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        manager: "gLJHKPrZLGBiBZ33hFgZh6YnsEhTVxuRT17UCqNp6ff",
-        fund: "4gAcSdfSAxVPcxj2Hi3AvKKViGat3iUysDD5ZzbqhDTk"
-      })
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ manager, fund })
     });
     const { tx } = await response.json();
-    console.log("unwrap tx", tx);
+    console.log("Unwrap tx", tx);
 
-    const t = Transaction.from(Buffer.from(tx, "hex"));
-    t.recentBlockhash = (
-      await glamClient.provider.connection.getLatestBlockhash()
-    ).blockhash;
-    console.log("unwrap recentBlockhash", t.recentBlockhash);
-    t.sign(glamClient.getWalletSigner());
     try {
-      const txId = await glamClient.provider.connection.sendRawTransaction(
-        t.serialize()
+      const txId = await sendAndConfirmTransaction(
+        glamClient.provider.connection,
+        Transaction.from(Buffer.from(tx, "hex")),
+        [glamClient.getWalletSigner()]
       );
-      console.log("Wrap txId:", txId);
+      console.log("Unwrap txId:", txId);
     } catch (error) {
       console.log("Error", error);
       throw error;
     }
-  });
-  */
+  }, 30_000);
 
-  it("jupiter swap", async () => {
+  it("Jupiter swap", async () => {
     const response = await fetch(`${API}/tx/jupiter/swap`, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        fund: "4gAcSdfSAxVPcxj2Hi3AvKKViGat3iUysDD5ZzbqhDTk",
-        manager: "gLJHKPrZLGBiBZ33hFgZh6YnsEhTVxuRT17UCqNp6ff",
+        fund,
+        manager,
         quote: {
           inputMint: wsol.toBase58(),
           outputMint: msol.toBase58(),
@@ -97,9 +81,7 @@ describe("glam_api_tx", () => {
         }
       })
     });
-    const { tx, versioned } = await response.json();
-    // console.log("is versioned:", versioned, "jupiter swap tx:", tx);
-
+    const { tx } = await response.json();
     const vTx = VersionedTransaction.deserialize(Buffer.from(tx, "hex"));
     try {
       const txId = await (
