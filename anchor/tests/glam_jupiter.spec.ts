7865dec48653bdab9b49ce7f8a9f8491b006251b
Emanuele Cesena <emanuele.cesena@gmail.com>
2024-06-16 18:23:15 -0700

    swap: add msol->wsol e2e test

--- anchor/tests/glam_jupiter.spec.ts
+++ anchor/tests/glam_jupiter.spec.ts
@@ -401,6 +401,208 @@ describe("glam_jupiter", () => {
     expect(treasuryMsol.amount.toString()).toEqual("41795954");
   });
 
+  it("Swap back end to end", async () => {
+    const swapInstructions = {
+      tokenLedgerInstruction: null,
+      computeBudgetInstructions: [
+        {
+          programId: "ComputeBudget111111111111111111111111111111",
+          accounts: [],
+          data: "AsBcFQA="
+        }
+      ],
+      setupInstructions: [
+        {
+          programId: "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL",
+          accounts: [
+            {
+              pubkey: "FsbYwp2VmCdouBbJLKTGo5Gf8RsUUQWVZzA1gvRbsk89",
+              isSigner: true,
+              isWritable: true
+            },
+            {
+              pubkey: "Ghk24stAfSCWywoUygdzkhWxZkXZHTmjBWaprkrC3EDh",
+              isSigner: false,
+              isWritable: true
+            },
+            {
+              pubkey: "FsbYwp2VmCdouBbJLKTGo5Gf8RsUUQWVZzA1gvRbsk89",
+              isSigner: false,
+              isWritable: false
+            },
+            {
+              pubkey: "So11111111111111111111111111111111111111112",
+              isSigner: false,
+              isWritable: false
+            },
+            {
+              pubkey: "11111111111111111111111111111111",
+              isSigner: false,
+              isWritable: false
+            },
+            {
+              pubkey: "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
+              isSigner: false,
+              isWritable: false
+            }
+          ],
+          data: "AQ=="
+        }
+      ],
+      swapInstruction: {
+        programId: "JUP6LkbZbjS1jKKwapdHNy74zcZ3tLUZoi5QNyVTaV4",
+        accounts: [
+          {
+            pubkey: "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
+            isSigner: false,
+            isWritable: false
+          },
+          {
+            pubkey: "FsbYwp2VmCdouBbJLKTGo5Gf8RsUUQWVZzA1gvRbsk89",
+            isSigner: true,
+            isWritable: false
+          },
+          {
+            pubkey: "2pqUNdx8Rvf63PQGTB3wMGnmGKjofBTrcQCyqgzbvKPP",
+            isSigner: false,
+            isWritable: false
+          },
+          {
+            pubkey: "Ghk24stAfSCWywoUygdzkhWxZkXZHTmjBWaprkrC3EDh",
+            isSigner: false,
+            isWritable: false
+          },
+          {
+            pubkey: "JUP6LkbZbjS1jKKwapdHNy74zcZ3tLUZoi5QNyVTaV4",
+            isSigner: false,
+            isWritable: false
+          },
+          {
+            pubkey: "So11111111111111111111111111111111111111112",
+            isSigner: false,
+            isWritable: false
+          },
+          {
+            pubkey: "JUP6LkbZbjS1jKKwapdHNy74zcZ3tLUZoi5QNyVTaV4",
+            isSigner: false,
+            isWritable: false
+          },
+          {
+            pubkey: "D8cy77BBepLMngZx6ZukaTff5hCt1HrWyKk3Hnd9oitf",
+            isSigner: false,
+            isWritable: false
+          },
+          {
+            pubkey: "JUP6LkbZbjS1jKKwapdHNy74zcZ3tLUZoi5QNyVTaV4",
+            isSigner: false,
+            isWritable: false
+          },
+          {
+            pubkey: "MERLuDFBMmsHnsBPZw2sDQZHvXFMwp8EdjudcU2HKky",
+            isSigner: false,
+            isWritable: false
+          },
+          {
+            pubkey: "MAR1zHjHaQcniE2gXsDptkyKUnNfMEsLBVcfP7vLyv7",
+            isSigner: false,
+            isWritable: false
+          },
+          {
+            pubkey: "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
+            isSigner: false,
+            isWritable: false
+          },
+          {
+            pubkey: "GcJckEnDiWjpjQ8sqDKuNZjJUKAFZSiuQZ9WmuQpC92a",
+            isSigner: false,
+            isWritable: false
+          },
+          {
+            pubkey: "FsbYwp2VmCdouBbJLKTGo5Gf8RsUUQWVZzA1gvRbsk89",
+            isSigner: false,
+            isWritable: false
+          },
+          {
+            pubkey: "2pqUNdx8Rvf63PQGTB3wMGnmGKjofBTrcQCyqgzbvKPP",
+            isSigner: false,
+            isWritable: true
+          },
+          {
+            pubkey: "Ghk24stAfSCWywoUygdzkhWxZkXZHTmjBWaprkrC3EDh",
+            isSigner: false,
+            isWritable: true
+          },
+          {
+            pubkey: "EWy2hPdVT4uGrYokx65nAyn2GFBv7bUYA2pFPY96pw7Y",
+            isSigner: false,
+            isWritable: true
+          },
+          {
+            pubkey: "GM48qFn8rnqhyNMrBHyPJgUVwXQ1JvMbcu3b9zkThW9L",
+            isSigner: false,
+            isWritable: true
+          }
+        ],
+        data: "5RfLl3rjrSoBAAAACmQAAUCccQIAAAAA8knsAgAAAAAyAAA="
+      },
+      cleanupInstruction: {
+        programId: "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
+        accounts: [
+          {
+            pubkey: "Ghk24stAfSCWywoUygdzkhWxZkXZHTmjBWaprkrC3EDh",
+            isSigner: false,
+            isWritable: true
+          },
+          {
+            pubkey: "FsbYwp2VmCdouBbJLKTGo5Gf8RsUUQWVZzA1gvRbsk89",
+            isSigner: false,
+            isWritable: true
+          },
+          {
+            pubkey: "FsbYwp2VmCdouBbJLKTGo5Gf8RsUUQWVZzA1gvRbsk89",
+            isSigner: true,
+            isWritable: false
+          }
+        ],
+        data: "CQ=="
+      },
+      otherInstructions: [],
+      addressLookupTableAddresses: [
+        "6TSS8bTxDjGitZgtTc7gdVWkzKdpGLb9QDi7kXAQQXXp"
+      ],
+      prioritizationFeeLamports: 0
+    };
+
+    // Swap
+    const amount = 41_000_000;
+    try {
+      const txId = await glamClient.jupiter.swap(
+        fundPDA,
+        {
+          inputMint: msol.toBase58(),
+          outputMint: wsol.toBase58(),
+          amount,
+          swapMode: "ExactIn",
+          onlyDirectRoutes: true,
+          maxAccounts: 8
+        },
+        undefined,
+        swapInstructions
+      );
+      console.log("swap back e2e txId", txId);
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+
+    // treasury: more mSOL
+    const treasuryMsol = await getAccount(
+      glamClient.provider.connection,
+      glamClient.getTreasuryAta(fundPDA, msol)
+    );
+    expect(treasuryMsol.amount.toString()).toEqual("795954");
+  });
+
   it("Swap by providing quote params", async () => {
     const amount = 50_000_000;
     try {
