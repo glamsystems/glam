5948d58ffa9e6a95d21e4b3876336698e0a584a2
Yuru Shao <shaoyuru@gmail.com>
2024-06-18 22:43:38 -0700

    anchor: add security_txt

---
+++ anchor/programs/glam/src/security_txt.rs
@@ -0,0 +1,14 @@
+pub mod security {
+    use solana_security_txt::security_txt;
+
+    #[cfg(not(feature = "no-entrypoint"))]
+    security_txt! {
+        name: "GLAM Protocol",
+        project_url: "https://www.glam.systems/",
+        policy: "https://gui.glam.systems/vault/disclaimer",
+        contacts: "email: hello@glam.systems",
+        preferred_languages: "en",
+        source_release: "v0.4.0",
+        source_code: "https://github.com/glamsystems/glam"
+    }
+}
