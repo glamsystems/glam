0f9412d47245a00fa3b47c5dd6356b9de77283cb
Yuru Shao <shaoyuru@gmail.com>
2024-07-13 21:38:41 -0700

    bump glam version to 0.2.4

--- anchor/target/types/glam.ts
+++ anchor/target/types/glam.ts
@@ -1,5 +1,5 @@
 export type Glam = {
-  "version": "0.2.3",
+  "version": "0.2.4",
   "name": "glam",
   "constants": [
     {
@@ -3523,7 +3523,7 @@ export type Glam = {
 };
 
 export const IDL: Glam = {
-  "version": "0.2.3",
+  "version": "0.2.4",
   "name": "glam",
   "constants": [
     {
