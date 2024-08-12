321a2b271661c0f03ca5ced145ff2357dbd4de7a
Fabio Federici <f@federici.xyz>
2024-08-12 14:59:03 +0200

    first step towards dynamic forms, created product and share class JSON schema

--- playground/src/app/products/page.tsx
+++ playground/src/app/products/page.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import { z } from "zod"
+
+const testSchema = z.object({ "x-id": z.any().optional(), "title": z.any().optional(), "description": z.any().optional(), "type": z.any().optional(), "x-component": z.any().optional() });
+
 export default function Products() {
-  return (
-    <div className="text-xl">Products</div>
-  );
+    return (
+        <div>
+            <p>Products</p>
+        </div>
+    );
 }
