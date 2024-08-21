036ab3d6e1e3bf1f899296aafb7dad2f42fc9ddb
Yuru Shao <shaoyuru@gmail.com>
2024-08-20 23:54:34 -0700

    [api] trim dependencies

--- anchor/src/index.react.ts
+++ anchor/src/index.react.ts
@@ -2,6 +2,7 @@ export * from "./glamExports";
 export * from "./models";
 export * from "./clientConfig";
 export * from "./client";
+export * from "./react";
 export * from "./utils/offchain";
 export * from "./utils/helpers";
 export * from "./constants";
