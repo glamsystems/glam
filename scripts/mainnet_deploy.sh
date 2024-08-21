bf2f89b07eaf66c51a5a06b17b94b41cf3f8d56c
Yuru Shao <shaoyuru@gmail.com>
2024-08-20 22:56:54 -0700

    Update mainnet deploy script

--- scripts/mainnet_deploy.sh
+++ scripts/mainnet_deploy.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-PROGRAM_KEYPAIR=~/solana-keypairs/glam/glam-keypair-GLAMpLuXu78TA4ao3DPZvT1zQ7woxoQ8ahdYbhnqY9mP.json
-SOLANA=~/code/agave/target/debug/solana
-PRIORITY_FEE=50000
+PROGRAM_KEYPAIR=
+SOLANA=
+PRIORITY_FEE=10000
 MAX_ATTEMPTS=1000
+PROGRAM_ID=GLAMpLuXu78TA4ao3DPZvT1zQ7woxoQ8ahdYbhnqY9mP
 
 build() {
     anchor build -- --features mainnet
@@ -19,7 +20,7 @@ deploy() {
     --max-sign-attempts $MAX_ATTEMPTS
 }
 
-if [ -f $SOLANA ]; then
+if [ -n "$SOLANA" ] && [ -f "$SOLANA" ]; then
     echo "✅ Found solana binary: $SOLANA"
 else
     which solana > /dev/null || (echo "❌ Missing solana binary" && exit 1)
@@ -27,9 +28,10 @@ else
     echo "✅ Use solana binary at $SOLANA"
 fi
 
-$SOLANA address -k $PROGRAM_KEYPAIR >> /dev/null || (echo "❌ Missing program keypair" && exit 1)
-if [ $($SOLANA address -k $PROGRAM_KEYPAIR) == "GLAMpLuXu78TA4ao3DPZvT1zQ7woxoQ8ahdYbhnqY9mP" ]; then
-    echo "✅ Program keypair is correct: $PROGRAM_KEYPAIR"
+$SOLANA address -k $PROGRAM_KEYPAIR &> /dev/null || (echo "❌ Missing program keypair" && exit 1)
+if [ $($SOLANA address -k $PROGRAM_KEYPAIR) == $PROGRAM_ID ]; then
+    echo "✅ Program keypair : $PROGRAM_KEYPAIR"
+    echo "✅ Program ID validated: $PROGRAM_ID"
 else
     echo "❌ Program keypair is incorrect" && exit 1
 fi
@@ -41,7 +43,13 @@ else
 fi
 
 # anchor build will remove program id from the idl file, we need to restore it
-(cd anchor && build) && git checkout anchor/target/idl/glam.json
+(cd anchor && build)
+
+size_kb=$(ls -lh anchor/target/deploy/glam.so | awk '{print $5}')
+size_bytes=$(ls -l anchor/target/deploy/glam.so | awk '{print $5}')
+echo "==== Program size ===="
+echo "glam.so: $size_kb ($size_bytes bytes)"
+echo "======================"
 
 # Uncomment the following line to deploy the program
 # deploy
