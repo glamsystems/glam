64f3f13bda6849497b586ef691d44d3b2f97901d
Fabio Federici <f@federici.xyz>
2024-10-13 12:28:25 +0200

    beta warning styling and mobile fix

--- playground/src/components/BetaWarning.tsx
+++ playground/src/components/BetaWarning.tsx
@@ -6,20 +6,21 @@ import Link from "next/link";
 
 const BetaWarning = () => {
   return (
-    <div className="fixed w-full pl-[244px] bottom-0 h-[56px] z-30 flex flex-row items-center justify-center bg-muted select-none">
+    <div className="fixed w-full pl-[260px] bottom-0 h-[56px] z-30 flex flex-row items-center justify-center bg-muted select-none sm:opacity-0 lg:opacity-100 pr-4">
       <div className="flex flex-row items-center justify-center w-full text-sm text-destructive">
         <ExclamationTriangleIcon className="w-5 h-5 mr-2" />
-        <p className="font-medium mr-2">Unaudited Beta</p>
-        <p className="leading-5 text-center text-muted-foreground">
+        <p className="font-medium mr-2 text-nowrap">Unaudited Beta</p>
+        <p className="leading-5 text-center text-muted-foreground truncate text-nowrap">
           Active development; breaking changes. Use at your own risk. Possible losses. No guarantees. GLAM not liable for any losses.{" "}
+        </p>
+        <p className="leading-5 text-center text-muted-foreground text-nowrap">
           <Link href="/disclaimer" className="underline hover:text-destructive transition-colors">
             See full disclaimer
           </Link>
           .
         </p>
       </div>
-    </div>
-  );
+    </div>);
 };
 
 export default BetaWarning;
