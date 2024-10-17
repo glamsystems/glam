f2fdfc310d9a92fae0c57409c04723dbd3089fde
Yuru Shao <shaoyuru@gmail.com>
2024-10-17 02:51:07 +0000

    Add comments

--- cli/src/main.ts
+++ cli/src/main.ts
@@ -1,7 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { GlamClient, MSOL, USDC, WSOL } from "@glam/anchor";
 import { TOKEN_2022_PROGRAM_ID } from "@solana/spl-token";
-import { PublicKey } from "@solana/web3.js";
+import { ComputeBudgetProgram, PublicKey } from "@solana/web3.js";
 
 const anchorProvider = anchor.AnchorProvider.env();
 
@@ -20,17 +20,18 @@ export const fund = {
       // Glam Token
       name: "Glam Fund SOL-mSOL",
       symbol: "GBS",
-      asset: USDC,
+      asset: WSOL,
       allowlist: [] as PublicKey[],
       blocklist: [] as PublicKey[],
+
       // Glam
-      lockUpTime: 4 * 60 * 60,
-      requiresMemoOnTransfer: true,
+      lockUpTime: 0,
+
       // Openfunds Share Class
       fullShareClassName: "Glam Fund SOL-mSOL",
-      isin: "XS1082172823",
-      cusip: "demo",
-      valor: "demo",
+      isin: "",
+      cusip: "",
+      valor: "",
       shareClassCurrency: "SOL",
       shareClassLifecycle: "active",
       investmentStatus: "open",
@@ -38,7 +39,7 @@ export const fund = {
       shareClassLaunchDate: new Date().toISOString().split("T")[0],
       minimalInitialSubscriptionCategory: "amount",
       minimalInitialSubscriptionInShares: "0",
-      minimalInitialSubscriptionInAmount: "1000",
+      minimalInitialSubscriptionInAmount: "1",
       currencyOfMinimalSubscription: "SOL",
       minimalRedemptionCategory: "shares",
       minimalInitialRedemptionInShares: "1",
@@ -47,26 +48,31 @@ export const fund = {
       maximumInitialRedemptionInAmount: null,
       currencyOfMinimalOrMaximumRedemption: "SOL",
       shareClassDividendType: "both",
-      srri: "4",
+      srri: "7",
       hasLockUpForRedemption: true,
-      lockUpComment: "demo",
-      lockUpPeriodInDays: "40",
-      launchPrice: "100",
-      launchPriceCurrency: "USD",
+      lockUpComment: "",
+      lockUpPeriodInDays: "0",
+      launchPrice: "1",
+      launchPriceCurrency: "SOL",
       launchPriceDate: new Date().toISOString().split("T")[0],
     },
   ],
+
   // Glam
   isEnabled: true,
+  assets: [WSOL, MSOL],
+  assetsWeights: [0, 100],
+
   // Openfunds (Fund)
   fundDomicileAlpha2: "XS",
   legalFundNameIncludingUmbrella: "Glam Fund SOL-mSOL",
   fundLaunchDate: new Date().toISOString().split("T")[0],
-  investmentObjective: "demo",
+  investmentObjective: "Glam Fund SOL-mSOL",
   fundCurrency: "SOL",
   openEndedOrClosedEndedFundStructure: "open-ended fund",
   fiscalYearEnd: "12-31",
   legalForm: "other",
+
   // Openfunds Company (simplified)
   company: {
     fundGroupName: "Glam Systems",
@@ -75,27 +81,30 @@ export const fund = {
     emailAddressOfManCo: "hello@glam.systems",
     fundWebsiteOfManCo: "https://glam.systems",
   },
+
   // Openfunds Manager (simplified)
   manager: {
     portfolioManagerName: "glam.sol",
   },
 };
-
 async function main(): Promise<void> {
+  // Update the fund object listed above before creating the fund
   /*
   const [txId, fundPDA] = await glamClient.createFund(fund);
   console.log("Fund PDA:", fundPDA.toBase58());
   console.log("txId:", txId);
+  */
 
-  const fundPDA = new PublicKey("G6AAkp7ZY3aAh8E1Nbpm6f87ngR68XN6KpBmopDJZXCb");
+  // Update the fund pubkey before closing the share class
+  /*
+  const fundPDA = new PublicKey("Dc88inhuwymwyvVR7Sy7MdeYyxsAvPaCgYZfAQsY3skJ");
   try {
     const txId = await glamClient.program.methods
       .closeShareClass(0)
       .accounts({
-        manager: glamClient.getManager(),
         fund: fundPDA,
-        shareClass: glamClient.getShareClassPDA(fundPDA, 0),
-        token2022Program: TOKEN_2022_PROGRAM_ID,
+        shareClassMint: glamClient.getShareClassPDA(fundPDA, 0),
+        openfunds: glamClient.getOpenfundsPDA(fundPDA),
       })
       .rpc();
     console.log("txId:", txId);
@@ -103,16 +112,17 @@ async function main(): Promise<void> {
     console.error(error);
     throw error;
   }
+  */
 
-  const fundPDA = new PublicKey("G6AAkp7ZY3aAh8E1Nbpm6f87ngR68XN6KpBmopDJZXCb");
+  // Update the fund pubkey before closing the fund
+  /*
+  const fundPDA = new PublicKey("Dc88inhuwymwyvVR7Sy7MdeYyxsAvPaCgYZfAQsY3skJ");
   try {
     const txId = await glamClient.program.methods
       .closeFund()
       .accounts({
-        manager: glamClient.getManager(),
         fund: fundPDA,
         openfunds: glamClient.getOpenfundsPDA(fundPDA),
-        treasury: glamClient.getTreasuryPDA(fundPDA),
       })
       .rpc();
     console.log("txId:", txId);
