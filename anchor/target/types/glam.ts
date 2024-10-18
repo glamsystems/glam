8f93d77522106e9b6f2e37287935544f1c003894
Yuru Shao <shaoyuru@gmail.com>
2024-10-18 04:05:27 +0000

    anchor: bump glam version to 0.3.4

--- anchor/target/types/glam.ts
+++ anchor/target/types/glam.ts
@@ -8,7 +8,7 @@ export type Glam = {
   "address": "GLAMpLuXu78TA4ao3DPZvT1zQ7woxoQ8ahdYbhnqY9mP",
   "metadata": {
     "name": "glam",
-    "version": "0.3.3",
+    "version": "0.3.4",
     "spec": "0.1.0",
     "description": "Glam Protocol"
   },
@@ -3869,63 +3869,13 @@ export type Glam = {
   "errors": [
     {
       "code": 6000,
-      "name": "fundNotActive",
-      "msg": "Fund is not active"
+      "name": "notAuthorized",
+      "msg": "Signer is not authorized"
     },
     {
       "code": 6001,
-      "name": "invalidShareClass",
-      "msg": "Share class not allowed to subscribe"
-    },
-    {
-      "code": 6002,
-      "name": "invalidAssetSubscribe",
-      "msg": "Asset not allowed to subscribe"
-    },
-    {
-      "code": 6003,
-      "name": "invalidPricingOracle",
-      "msg": "Invalid oracle for asset price"
-    },
-    {
-      "code": 6004,
-      "name": "invalidRemainingAccounts",
-      "msg": "Invalid accounts: the transaction is malformed"
-    },
-    {
-      "code": 6005,
-      "name": "invalidTreasuryAccount",
-      "msg": "Invalid treasury ata"
-    },
-    {
-      "code": 6006,
-      "name": "invalidSignerAccount",
-      "msg": "Invalid signer ata"
-    },
-    {
-      "code": 6007,
-      "name": "invalidAssetPrice",
-      "msg": "Invalid asset price"
-    },
-    {
-      "code": 6008,
-      "name": "invalidStableCoinPriceForSubscribe",
-      "msg": "Subscription not allowed: invalid stable coin price"
-    },
-    {
-      "code": 6009,
-      "name": "subscribeRedeemPaused",
-      "msg": "Fund is paused for subscription and redemption"
-    },
-    {
-      "code": 6010,
-      "name": "invalidPolicyAccount",
-      "msg": "Policy account is mandatory"
-    },
-    {
-      "code": 6011,
-      "name": "priceTooOld",
-      "msg": "Price is too old"
+      "name": "integrationDisabled",
+      "msg": "Integration is disabled"
     }
   ],
   "types": [
