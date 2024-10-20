ee219613ee12983cf691dbd682a596bf1feb4bfb
Yuru Shao <shaoyuru@gmail.com>
2024-10-19 20:29:47 -0700

    pg: show skeleton during price loading

--- playground/src/app/page.tsx
+++ playground/src/app/page.tsx
@@ -28,7 +28,6 @@ export default function Products() {
 
   return (
     <PageContentWrapper>
-      {/*@ts-ignore*/}
       <DataTable data={products} columns={columns} isLoading={isLoading} />
     </PageContentWrapper>
   );
