075ba782c3d63c76c608a5f4e74b8b960775bd66
Yuru Shao <shaoyuru@gmail.com>
2024-10-31 22:59:54 -0700

    anchor: bump glam version to 0.3.5

--- anchor/target/types/glam.ts
+++ anchor/target/types/glam.ts
@@ -8,7 +8,7 @@ export type Glam = {
   "address": "GLAMpLuXu78TA4ao3DPZvT1zQ7woxoQ8ahdYbhnqY9mP",
   "metadata": {
     "name": "glam",
-    "version": "0.3.4",
+    "version": "0.3.5",
     "spec": "0.1.0",
     "description": "Glam Protocol"
   },
