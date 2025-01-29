6800aa3c858918b96b0871b7d1dc4c3ce1c6666a
Yuru Shao <shaoyuru@gmail.com>
2025-01-28 22:30:36 -0800

    Cherry-pick to fix gui with GLAMpLuXu78TA4ao3DPZvT1zQ7woxoQ8ahdYbhnqY9mP

--- anchor/src/client/state.ts
+++ anchor/src/client/state.ts
@@ -70,6 +70,9 @@ export class StateClient {
         .instruction();
 
       const shareClassMint = this.base.getShareClassPda(statePda, 0);
+      // FIXME: setting rawOpenfunds to null is a workarond for
+      // Access violation in stack frame 5 at address 0x200005ff8 of size 8
+      mints[0].rawOpenfunds = null;
       const txSig = await this.base.program.methods
         .addShareClass(mints[0])
         .accounts({
